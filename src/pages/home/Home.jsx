c88f9fbde31a9344ff27a58c79802e673fcd67b0
Allen James D. <allenjamesdatuin@gmail.com>
2025-07-26 21:30:34 +0800

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,9 @@ import Hero from './hero/Hero';
 import Category from './category/Category';
 import Search from './search/Search';
 import Services from './services/Services';
-import Cars from './cars/Cars'; 
+import Cars from './cars/Cars';
+import Banner from './banner/Banner';
+import Blogs from './blog/Blogs'; 
 
 const Home = () => {
   return (
@@ -23,8 +25,11 @@ const Home = () => {
       {/* Car Search */}
       <Search />
 
+      {/* Banner (Events)*/}
+      <Banner />
 
-
+      {/* Blogs */}
+      <Blogs />
     </div>
   );
 };
