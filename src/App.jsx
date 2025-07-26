f0c5791a35fc700a78868bac014209391154980d
Allen James D. <allenjamesdatuin@gmail.com>
2025-07-26 21:28:07 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home';
-import About from "./pages/home/about/About"; // adjust path based on file structure
-import Cars from './pages/home/cars/Cars'; // adjust path based on file structure
-import Services from './pages/home/services/Services'; // adjust path based on file structure
-import Inventory from './pages/home/search/Search'; // adjust path based on file structure
+import About from "./pages/home/about/About"; 
+import Cars from './pages/home/cars/Cars'; 
+import Services from './pages/home/services/Services'; 
+import Inventory from './pages/home/search/Search'; 
+import Events from "./pages/home/events/Events";
+import Blogs from './pages/home/blog/Blogs'; 
 
 
 import Footer from './components/footer/Footer';
@@ -24,12 +26,14 @@ function App() {
             <Route path="/cars" element={<Cars />} />
             <Route path="/services" element={<Services />} />
             <Route path="/inventory" element={<Inventory />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/blogs" element={<Blogs />} />
+           
             {/* Add more routes as needed */}
           </Routes>
 
           {/* Footer */}
           <Footer />
-          
         </main>
       </Router>
     </>
