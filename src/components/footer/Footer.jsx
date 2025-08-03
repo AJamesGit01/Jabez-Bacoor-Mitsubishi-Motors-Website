86fe4871b6e4c570fc7817894380ab67ea3d8a49
Allen James D. <allenjamesdatuin@gmail.com>
2025-08-03 13:09:04 +0800

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,6 @@ import logo from '../../assets/logo.png'; // Mitsubishi Logo
 
 const Footer = () => {
   return ( 
-    
 
 <footer className="bg-zinc-900 py-6">
     <div className="w-full max-w-screen-2xl mx-auto py-4 md:px-12 px-3 md:py-8">
@@ -15,16 +14,16 @@ const Footer = () => {
             </Link>
             <ul className="flex flex-wrap items-center mb-6 text-base font-normal sm:mb-0 text-zinc-400">
                 <li>
-                    <Link to="/" className="hover:text-red-600 ease-in-out duration-300 me-4 md:me-6">About</Link>
+                    <Link to="https://www.mitsubishi-motors.com.ph/cars" className="hover:text-red-600 ease-in-out duration-300 me-4 md:me-6" target='_blank'>All Vehicles</Link>
                 </li>
                 <li>
-                    <Link to="/" className="hover:text-red-600 ease-in-out duration-300 me-4 md:me-6">Privacy Policy</Link>
+                    <Link to="https://www.mitsubishi-motors.com.ph/service/service-guide-0" className="hover:text-red-600 ease-in-out duration-300 me-4 md:me-6" target='_blank'>Service</Link>
                 </li>
                 <li>
-                    <Link to="/" className="hover:text-red-600 ease-in-out duration-300 me-4 md:me-6">Licensing</Link>
+                    <Link to="https://www.mitsubishi-motors.com.ph/price-list" className="hover:text-red-600 ease-in-out duration-300 me-4 md:me-6" target='_blank'>Prices</Link>
                 </li>
                 <li>
-                    <Link to="/" className="hover:text-red-600 ease-in-out duration-300 me-4 md:me-6">Contact</Link>
+                    <Link to="https://www.mitsubishi-motors.com.ph/privacy-policy" className="hover:text-red-600 ease-in-out duration-300 me-4 md:me-6" target='_blank'>Privacy Policy</Link>
                 </li>
             </ul>
         </div>
