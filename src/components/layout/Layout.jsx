37fcdf8ff899a95945f837ef663eba3ca5457426
Allen James D. <allenjamesdatuin@gmail.com>
2025-08-05 19:24:29 +0800

    Update Layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
-const Layout = ({ children, className }) => {
+const Layout = ({ children, className = "" }) => {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
+  const defaultPadding = isHome ? "" : "pt-20 px-4";
+
   return (
-    <div className={`w-full max-w-screen-2xl py-4 md:px-12 px-3 ${className}`}>
+    <div className={`w-full max-w-screen-2xl py-4 md:px-12 px-3 ${defaultPadding} ${className}`}>
       {children}
     </div>
   );
